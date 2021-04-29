57c239e7edff66a826b555f4c6c0389139b269bc
Felt0 <pldrzewiec@gmail.com>
2021-04-29 08:18:46 +0200

    tytuł

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ function dodajProdukt(typ,cena)
 {
     suma += Number(cena);
     document.getElementById("prod").innerHTML += "<p>"+typ+": "+cena+"zł</p>";
+    //add button to destroy object in html with id
     document.getElementById("suma").innerHTML = suma+"zł";
 }
